1abb710aa9fb277cfc755ad070e652c2401357cb
Tran Vuong Duy <tranvuongduy2003@gmail.com>
2025-02-17 00:37:14 +0700

    Set up projects

---
+++ test/DMS.DomainTests/UnitTest1.cs
@@ -0,0 +1,11 @@
+﻿namespace DMS.DomainTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ test/DMS.Infrastructure/UnitTest1.cs
@@ -0,0 +1,11 @@
+﻿namespace DMS.Infrastructure
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
